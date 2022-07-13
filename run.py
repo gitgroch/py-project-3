b852f7d78950f7607ebd2464911aefa2ab91c89a
gitgroch <geoff.rochford@gmail.com>
2022-07-13 10:49:43 +0000

    Set up main game loop and some end conditions

--- run.py
+++ run.py
@@ -1,3 +1,16 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+#setting up main game loop and end game conditions 
+if  __name__ == '__main__':
+    #Win condition 
+    #variable for game word
+    WORD == "TESTS"
+    try:
+        while True:
+            #variable for guess word
+            GUESS = "BREAD"
+            if WORD == GUESS:
+                print("Well done, you've won!")
+                break 
+    #exit condition 
+    except KeyboardInterrupt:
+        print("Game Ended.")  
