bb1f2b1453e68d45a5add48d486f14bfd0042bf8
gitgroch <geoff.rochford@gmail.com>
2022-07-13 11:03:21 +0000

    fix minor syntax errors

--- run.py
+++ run.py
@@ -1,16 +1,16 @@
-
-#setting up main game loop and end game conditions 
-if  __name__ == '__main__':
-    #Win condition 
-    #variable for game word
+# Identify module as main
+# setting up main game loop and end game conditions
+if __name__ == '__main__':
+    # win condition
+    # variable for game word
     WORD == "TESTS"
     try:
         while True:
-            #variable for guess word
+        # variable for guess word
             GUESS = "BREAD"
             if WORD == GUESS:
                 print("Well done, you've won!")
-                break 
-    #exit condition 
+                break
+    # exit condition
     except KeyboardInterrupt:
-        print("Game Ended.")  
+        print("Game Ended.")
