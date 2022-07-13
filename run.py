f10bd8dc949876ef4a96b34819dfa26c48588a54
gitgroch <geoff.rochford@gmail.com>
2022-07-13 13:40:23 +0000

    AAdd compare function for guesses vs game words

--- run.py
+++ run.py
@@ -1,21 +1,25 @@
 import typing
 
-# Identify module as main
-# setting up main game loop and end game conditions
-if __name__ == '__main__':
-    # win condition
-    # variable for game word
-    WORD == "TESTS"
-    try:
-        while True:
-        # variable for guess word
-            GUESS = "BREAD"
-            if WORD == GUESS:
-                print("Well done, you've won!")
-                break
-    # exit condition
-    except KeyboardInterrupt:
-        print("Game Ended.")
+def validate(guess: str, wordlen: int, wordlist: typing.Set[str]) -> typing.Tuple[str,str]:
+    """
+    Validate a guess from the player.
+    Takes in guess as string, length of game word and list of possible game words.
+    Returns tuple of any error and the guess word converted to upper case.
+    If no error is detected, None is returned for error.
+    """
+    # change guess into upper case if not already
+    guess_upper = guess.upper()
+    # if the length of the guess is not equal to the game word 
+    # length return validation message
+    if len(guess_upper) != wordlen:
+        # return a tuple consisting of error and the guess word
+        return f"Oops! Guess must be a length of {wordlen}, try again", guess_upper
+    # if the guess is not in the wordlist of valid word guesses, return validation error message
+    if guess_upper not in wordlist:
+        # return a tuple consisting of error and the guess word
+        return "Hmm, your guess doesn't appear to be a recognised word, try again.", guess_upper
+    # if no error detected return tuple of None and the guess word  
+    return None, guess_upper
 
 def get_player_guess(wordlen: int, worlist: typing.Set[str]) -> str:
     """
@@ -39,23 +43,19 @@ def get_player_guess(wordlen: int, worlist: typing.Set[str]) -> str:
     
     return guess
 
-def validate(guess: str, wordlen: int, wordlist: typing.Set[str]) -> typing.Tuple[str,str]:
-    """
-    Validate a guess from the player.
-    Takes in guess as string, length of game word and list of possible game words.
-    Returns tuple of any error and the guess word converted to upper case.
-    If no error is detected, None is returned for error.
-    """
-    # change guess into upper case if not already
-    guess_upper = guess.upper()
-    # if the length of the guess is not equal to the game word 
-    # length return validation message
-    if len(guess_upper) != wordlen:
-        # return a tuple consisting of error and the guess word
-        return f"Oops! Guess must be a length of {wordlen}, try again", guess_upper
-    # if the guess is not in the wordlist of valid word guesses, return validation error message
-    if guess_upper not in wordlist:
-        # return a tuple consisting of error and the guess word
-        return "Hmm, your guess doesn't appear to be a recognised word, try again.", guess_upper
-    # if no error detected return tuple of None and the guess word  
-    return None, guess_upper
+# Identify module as main
+# setting up main game loop and end game conditions
+if __name__ == '__main__':
+    # win condition
+    # variable for game word
+    WORD = "TESTS"
+    try:
+        while True:
+        # variable for guess word
+            GUESS = "BREAD"
+            if WORD == GUESS:
+                print("Well done, you've won!")
+                break
+    # exit condition
+    except KeyboardInterrupt:
+        print("   Game Ended.")
